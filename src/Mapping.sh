80882a83266f3bfebe4d4d8a5f73f6cfe27480b0
EwenLelias <ewen.lelias@ens-lyon.fr>
2021-11-19 08:53:15 +0000

    day 3 code bon

--- src/Mapping.sh
+++ src/Mapping.sh
@@ -19,11 +19,44 @@ for fq in ~/mydatalocal/NGS_epigenomics/processed_data/Trim/trimmed/*1_paired.*
   suffixe=${fq%%1_paired.fastq.gz} # enlever suffix
   prefixe2=${suffixe/"Trim/trimmed"/"mapping"} #remplace le suffixe
   #filed=processed_data/Trim/trimmed/${prefixe2}
-  bowtie2 -x ~/mydatalocal/NGS_epigenomics/processed_data/index/TAIR10 \
-  -1 ${suffixe}1_paired.fastq.gz -2 ${suffixe}2_paired.fastq.gz --threads 6\
+  head ${suffixe}1_paired.fastq.gz
+  echo bowtie2 -x ~/mydatalocal/NGS_epigenomics/processed_data/index/TAIR10 \
+  -1 ${suffixe}1_paired.fastq.gz -2 ${suffixe}2_paired.fastq.gz\
+  -U -1 ${suffixe}1_unpaired.fastq.gz -2 ${suffixe}2_unpaired.fastq.gz --threads 6\
   -X 2000  --very-sensitive "|" samtools sort -O BAM -o ${prefixe2}sorted.bam --threads 6 # le -x est le génome indexé
   #le -U est pour dire si il y a des données unpaired ici pas de -U
   # -X 2000
   done
 
 
+
+for fq in ~/mydatalocal/NGS_epigenomics/processed_data/Trim/trimmed/article/*1_paired.fastq  
+  do
+  #echo $fq
+  suffixe=${fq%%1_paired.fastq} # enlever suffix
+  prefixe2=${suffixe/"Trim/trimmed/article"/"mapping"} #remplace le suffixe
+  #filed=processed_data/Trim/trimmed/${prefixe2}
+  #zcat ${suffixe}1_paired.fastq.gz | head
+  bowtie2 -x ~/mydatalocal/NGS_epigenomics/processed_data/index/TAIR10 \
+  -1 ${suffixe}1_paired.fastq -2 ${suffixe}2_paired.fastq --threads 6\
+  -X 2000  --very-sensitive | samtools sort -O BAM -o ${prefixe2}sorted.bam --threads 6 # le -x est le génome indexé
+  #le -U est pour dire si il y a des données unpaired ici pas de -U
+  # -X 2000
+  done
+
+
+
+#zipper
+for fq in ~/mydatalocal/NGS_epigenomics/processed_data/Trim/trimmed/non_published/*1_paired.fastq.gz  
+  do
+  echo $fq
+  suffixe=${fq%%1_paired.fastq.gz} # enlever suffix
+  prefixe2=${suffixe/"Trim/trimmed/non_published"/"mapping"} #remplace le suffixe
+  #filed=processed_data/Trim/trimmed/${prefixe2}
+  #zcat ${suffixe}1_paired.fastq.gz | head
+  bowtie2 -x ~/mydatalocal/NGS_epigenomics/processed_data/index/TAIR10 \
+  -1 ${suffixe}1_paired.fastq.gz -2 ${suffixe}2_paired.fastq.gz --threads 6\
+  -X 2000  --very-sensitive | samtools sort -O BAM -o ${prefixe2}sorted.bam --threads 6 # le -x est le génome indexé
+  #le -U est pour dire si il y a des données unpaired ici pas de -U
+  # -X 2000
+  done
