57aff39983e6d12a9cc633a8cfc7438f28670859
EwenLelias <ewen.lelias@ens-lyon.fr>
2021-12-15 13:26:57 +0000

    j-1

--- src/Peak_Calling.sh
+++ src/Peak_Calling.sh
@@ -1,2 +1,48 @@
-TAIR=~/mydatalocal/NGS_epigenomics/data
+DIR=~/mydatalocal/NGS_epigenomics/processed_data/duplicate
+outDIR=~/mydatalocal/NGS_epigenomics/processed_data/Peak
+
+#remarque faire le modèle pour l'atac sec c'est pas util, seulement pour chip seq
+# il n'y a pas de distribution bimodale pour la taqseq
+
+mkdir -p ${outDIR}
+
+cd $outDIR
+
+#exsize = taille du fragment
+#shift permet de recentrer les sites de coupures en shiftant la valeur du centre
+#du read vers les côtés ou s'est effectivement passé la coupure. Souvent, on prend
+#exsize = 2 * shift
+
+bamsuffix=sortedmarked_duplicate_filtered.bam
+
+for f in ${DIR}/*_duplicate_filtered.bam
+
+do
+  idbase="$(basename -- $f)"
+  ID="${idbase//$bamsuffix/}"
+#  echo $ID
+  #  macs2 callpeak -t $f --outDIR ${outDIR}/${idbase} --nomodel --broad
+#  macs2 callpeak -t {$f} -n {$idbase} --outdir {$outDIR} -f {BAM}\
+ # -g hs -q 0.01 --nomodel --shift -75 --extsize 150 --keep-dup all -B --SPMR
+#  macs2 callpeak -t ${f} -n ${ID} --outdir ${outDIR} \
+#  -g 10e7 -q 0.01 --nomodel --shift -25 --extsize 50 --keep-dup all -B --SPMR --broad
+  macs2 callpeak -f "BAM" -t ${f} -n ${ID} --outdir ${outDIR}\
+  -q 0.01 --nomodel --shift -25 --extsize 50 --keep-dup "all" -B --broad --broad-cutoff 0.01 -g 10E7
+#Rscript ${outDIR}/NA_model.r
+done
+
+cd ${DIR}
+for f in ${DIR}/*filtered.bam
+do
+  samtools index $f
+done
+
+#https://bedtools.readthedocs.io/en/latest/content/tools/closest.html
+
+
+
+#BAM regarder les extrêmités 5' des 
+
+#tille génome arabidopisis  10e7
+
 
---
+++ src/bedtool_clothest.sh
@@ -0,0 +1,49 @@
+DIR=~/mydatalocal/NGS_epigenomics/data/
+outDIR=~/mydatalocal/NGS_epigenomics/processed_data/Peak
+
+gtf=${DIR}/Arabidopsis_thaliana.TAIR10.51.gtf
+
+gtf_filtered=${gtf/.gtf/.filtered.gtf}
+
+grep -v "^[MtPt]" ${gtf} | awk '{ if ($3=="gene") print $0 }' |\
+awk ' BEGIN { FS=OFS="\t" } { split($9,a,";") ; match(a[1], /AT[0-9]G[0-9]+/) ; id=substr(a[1],RSTART,RLENGTH) ; print $1,$4,$5,id,$7 }' |\
+sort -k1,1 -k2,2n > ${gtf_filtered}
+#get filtered est le fichier du génomes dont on ne garde que les gènes nucléaires (1er ligne nucléaire puis gène uniquemetn)
+#dernière ligne on trie pour plus de rapidité
+
+
+
+
+#BEDTOOLS
+
+for f in ${outDIR}/*broadPeak
+do
+  #echo ${f/_peaks.broadPeak/}.nearest.genes.txt
+  bedtools closest -a $f -b ${gtf_filtered} -D ref > ${f/_peaks.broadPeak/}.nearest.genes.txt
+done
+#D-ref pour savoir si overlap ou si plus loin
+# find the closest, non-overlapping gene for each interval where
+# both experiments had a peak
+# -io ignores overlapping intervals and returns only the closest,
+# non-overlapping interval (in this case, genes)
+
+#permet de savoir quels gène sont possiblement exprimés car accessible
+
+#cd $outDIR
+#do
+  #echo ${f/_peaks.broadPeak/}.nearest.genes.txt
+ # bedtools intersect -a ${outDIR}/2019_006_S6_R_peaks -b ${outDIR}/NOM AUTRE FICHIER QUON COMPARE -D ref > ${f/_peaks.broadPeak/}intersect.genes.txt
+#done
+
+###comparer avec bedtools intersect chaque fichier WOX5 donc quiescentes souches avec 374 de la racine
+mkdir -p $outDIR
+
+#pour 006 quiescent/racine 374
+WOX5=2019_006_S6_R_peaks.broadPeak
+racine=2020_374_S4.corrected_peaks.broadPeak
+
+#head $racine
+
+bedtools intersect -v -a ${outDIR}/$WOX5 -b ${outDIR}/$racine -wa > ${outDIR}/${WOX5}_${racine}_difference.txt
+
+
