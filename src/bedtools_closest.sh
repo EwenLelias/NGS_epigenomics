da99c54fb4a1c366d28f77780c6996d4e0192ef7
EwenLelias <ewen.lelias@ens-lyon.fr>
2021-12-22 10:57:59 +0000

    bedtools closest

--- src/bedtools_closest.sh
+++ src/bedtools_closest.sh
@@ -5,10 +5,14 @@ gtf=${DIR}/Arabidopsis_thaliana.TAIR10.51.gtf
 
 gtf_filtered=${gtf/.gtf/.filtered.gtf}
 
+#gtf ==> coordonnées, nom et orientation du gene, filtre le gtf avant
+
+
 grep -v "^[MtPt]" ${gtf} | awk '{ if ($3=="gene") print $0 }' |\
 awk ' BEGIN { FS=OFS="\t" } { split($9,a,";") ; match(a[1], /AT[0-9]G[0-9]+/) ; id=substr(a[1],RSTART,RLENGTH) ; print $1,$4,$5,id,$7 }' |\
 sort -k1,1 -k2,2n > ${gtf_filtered}
-#get filtered est le fichier du génomes dont on ne garde que les gènes nucléaires (1er ligne nucléaire puis gène uniquemetn)
+#gtf_filtered est le fichier du génomes dont on ne garde que les gènes nucléaires
+#1er ligne nucléaire on enlève les gènes Mt et Pt, puis on ne garde que les gènes uniquement dans le chromosome nucléaire
 #dernière ligne on trie pour plus de rapidité
 
 
@@ -22,12 +26,12 @@ do
   bedtools closest -a $f -b ${gtf_filtered} -D ref > ${f/_peaks.broadPeak/}.nearest.genes.txt
 done
 #D-ref pour savoir si overlap ou si plus loin
-# find the closest, non-overlapping gene for each interval where
-# both experiments had a peak
-# -io ignores overlapping intervals and returns only the closest,
-# non-overlapping interval (in this case, genes)
+#find the closest, non-overlapping gene for each interval where
+#both experiments had a peak
+#-io ignores overlapping intervals and returns only the closest,
+#non-overlapping interval (in this case, genes)
 
-#permet de savoir quels gène sont possiblement exprimés car accessible
+#permet de savoir quels gène sont possiblement exprimés car au niveau des régions accessibles
 
 #cd $outDIR
 #do
@@ -38,23 +42,23 @@ done
 ###comparer avec bedtools intersect chaque fichier WOX5 donc quiescentes souches avec 374 de la racine
 mkdir -p $outDIR
 
-#pour 006 quiescent/racine 374
+#pour les piques 006 quiescent et racine 374
 WOX5=2019_006_S6_R_peaks.broadPeak
 racine=2020_374_S4.corrected_peaks.broadPeak
 
 #head $racine
 
 bedtools intersect -v -a ${outDIR}/$WOX5 -b ${outDIR}/$racine -wa > ${outDIR}/${WOX5}_${racine}_difference.txt
+# v permet de ne sélectionner que les régions qui sont uniquement accessible en a ici a = le gènome accessbile de la cellule quiescente
 
-
-#pour 006 quiescent/racine 374
+#pour les gènes 006 quiescent et racine 374
 WOX5_genes=2019_006_S6_R.nearest.genes.txt
 racine_genes=2020_374_S4.corrected_.nearest.genes.txt
 
 #head $racine
 
 bedtools intersect -v -a ${outDIR}/${WOX5_genes} -b ${outDIR}/${racine_genes} -wa > ${outDIR}/${WOX5_genes}_${racine_genes}_difference.txt
-#v on regarde les gènes communs à a.
+#v permet de ne sélectionner que les gènes proches des régions accessibles uniquement à a, avec a = les gènes potentiellement accessible de la cellule quiescente
 
 #################################
 
